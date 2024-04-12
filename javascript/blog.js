6d4e493f67594a2c13382b93f55c0f8859c3ad17
Daniel Miller <102949058+AGuyNamedDan008@users.noreply.github.com>
2024-04-12 13:24:07 +0100

    Blog implementation

---
+++ javascript/blog.js
@@ -0,0 +1,143 @@
+let blogName = "Webpage-1990-Colourised";
+let authorName = "Daniel Miller";
+let authorLink = "https://webpage-1990-colourised.neocities.org";
+
+let postsArray = [
+[ "posts/2024-04-07-My-Fourth-Post-Example.html"],
+[ "posts/2024-04-06-My-Third-Post-Example.html"],
+[ "posts/2024-04-05-My-Second-Post-Example.html"],
+[ "posts/2024-04-04-Post-Template.html"] ];
+
+let url = window.location.pathname;
+const postDateFormat = /\d{4}\-\d{2}\-\d{2}\-/;
+
+let relativePath = ".";
+if ( url.includes("posts/") ) {
+  relativePath = "..";
+}
+
+let footerHTML = "<p>" + blogName + " is written by <a href='" + authorLink + "'>" + authorName + "</a>, built with <a href='https://zonelets.net/'>Zonelets</a>, and hosted by <a href='https://neocities.org/'>Neocities!</a></p>";
+
+let currentIndex = -1;
+let currentFilename = url.substring(url.lastIndexOf('posts/'));
+if ( ! currentFilename.endsWith(".html") ) {
+    currentFilename += ".html";
+}
+let i;
+for (i = 0; i < postsArray.length; i++) {
+  if ( postsArray[i][0] === currentFilename ) {
+    currentIndex = i;
+  }
+}
+
+function formatPostTitle(i) {
+  if ( postsArray[i].length > 1 ) {
+    return decodeURI(postsArray[i][1]);
+  } else { 
+	if (  postDateFormat.test ( postsArray[i][0].slice( 6,17 ) ) ) {
+	  return postsArray[i][0].slice(17,-5).replace(/-/g," ");
+    } else {
+      return postsArray[i][0].slice(6,-5).replace(/-/g," ");
+    }
+  }
+}
+
+let currentPostTitle = "";
+let niceDate = "";
+if ( currentIndex > -1 ) {
+  currentPostTitle = formatPostTitle( currentIndex );
+  if (  postDateFormat.test ( postsArray[currentIndex][0].slice( 6,17 ) ) ) {
+    let monthSlice = postsArray[currentIndex][0].slice( 11,13 );
+    let month = "";
+    if ( monthSlice === "01") { month = "Jan";}
+    else if ( monthSlice === "02") { month = "Feb";}
+    else if ( monthSlice === "03") { month = "Mar";}
+    else if ( monthSlice === "04") { month = "Apr";}
+    else if ( monthSlice === "05") { month = "May";}
+    else if ( monthSlice === "06") { month = "Jun";}
+    else if ( monthSlice === "07") { month = "Jul";}
+    else if ( monthSlice === "08") { month = "Aug";}
+    else if ( monthSlice === "09") { month = "Sep";}
+    else if ( monthSlice === "10") { month = "Oct";}
+    else if ( monthSlice === "11") { month = "Nov";}
+    else if ( monthSlice === "12") { month = "Dec";}
+	niceDate = postsArray[currentIndex][0].slice( 14,16 ) + " " + month + ", " + postsArray[currentIndex][0].slice( 6,10 );
+  }
+}
+
+function formatPostLink(i) {
+  let postTitle_i = "";
+  if ( postsArray[i].length > 1 ) {
+    postTitle_i = decodeURI(postsArray[i][1]);
+  } else {
+	if (  postDateFormat.test ( postsArray[i][0].slice( 6,17 ) ) ) {
+	  postTitle_i = postsArray[i][0].slice(17,-5).replace(/-/g," ");
+    } else {
+      postTitle_i = postsArray[i][0].slice(6,-5).replace(/-/g," ");
+    }
+  }
+  if (  postDateFormat.test ( postsArray[i][0].slice( 6,17 ) ) ) {
+    return '<li><a href="' + relativePath + '/'+ postsArray[i][0] +'">' + postsArray[i][0].slice(6,16) + " \u00BB " + postTitle_i + '</a></li>';
+  } else {
+    return '<li><a href="' + relativePath + '/'+ postsArray[i][0] +'">' + postTitle_i + '</a></li>';
+  }
+}
+
+let postListHTML = "<ul>";
+for ( let i = 0; i < postsArray.length; i++ ) {
+  postListHTML += formatPostLink(i);
+}
+postListHTML += "</ul>";
+
+let recentPostsCutoff = 3;
+let recentPostListHTML = "<h1>Recent Posts:</h1><ul>";
+let numberOfRecentPosts = Math.min( recentPostsCutoff, postsArray.length );
+for ( let i = 0; i < numberOfRecentPosts; i++ ) {
+  recentPostListHTML += formatPostLink(i);
+}
+
+if ( postsArray.length > recentPostsCutoff ) {
+  recentPostListHTML += '<li class="moreposts"><a href=' + relativePath + '/archive.html>\u00BB more posts</a></li></ul>';
+} else {
+  recentPostListHTML += "</ul>";
+}
+
+let nextprevHTML = "";
+let nextlink = "";
+let prevlink = "";
+
+if ( postsArray.length < 2 ) {
+  nextprevHTML = '<a href="' + relativePath + '/blog.html">Home</a>';
+} else if ( currentIndex === 0 ) {
+  prevlink = postsArray[currentIndex + 1][0];
+  nextprevHTML = '<a href="' + relativePath + '/blog.html">Home</a> | <a href="'+ relativePath + '/' + prevlink +'">Previous Post \u00BB</a>';
+} else if ( currentIndex === postsArray.length - 1 ) {
+  nextlink = postsArray[currentIndex - 1][0];
+  nextprevHTML = '<a href="' + relativePath + '/' + nextlink +'">\u00AB Next Post</a> | <a href="' + relativePath + '/blog.html">Home</a>';
+} else if ( 0 < currentIndex && currentIndex < postsArray.length - 1 ) {
+  nextlink = postsArray[currentIndex - 1][0];
+  prevlink = postsArray[currentIndex + 1][0];
+  nextprevHTML = '<a href="' + relativePath + '/'+ nextlink +'">\u00AB Next Post</a> | <a href="' + relativePath + '/blog.html">Home</a> | <a href="' + relativePath + '/'+ prevlink +'">Previous Post \u00BB</a>';
+}
+
+if (document.getElementById("nextprev")) {
+  document.getElementById("nextprev").innerHTML = nextprevHTML;
+}
+if (document.getElementById("postlistdiv")) {
+  document.getElementById("postlistdiv").innerHTML = postListHTML;
+}
+if (document.getElementById("recentpostlistdiv")) {
+  document.getElementById("recentpostlistdiv").innerHTML = recentPostListHTML;
+}
+if (document.getElementById("blogTitleH1")) {
+  document.getElementById("blogTitleH1").innerHTML = blogTitle;
+}
+if (document.getElementById("postTitleH1")) {
+  document.getElementById("postTitleH1").innerHTML = currentPostTitle;
+}
+if (document.getElementById("postDate")) {
+  document.getElementById("postDate").innerHTML = niceDate;
+}
+if (document.getElementById("footer")) {
+  document.getElementById("footer").innerHTML = footerHTML;
+}
